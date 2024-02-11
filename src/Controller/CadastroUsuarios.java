c0ca1f3a06494c4fdbe900b63bcf49c46b41624e
StudyJPA <contatovct777@gmail.com>
2024-02-10 22:46:29 -0300

    Atualizão das funções

--- src/Controller/CadastroUsuarios.java
+++ src/Controller/CadastroUsuarios.java
@@ -5,19 +5,22 @@ import java.util.ArrayList;
 
 // parte de cadastro de usuario
 public class CadastroUsuarios {
-    private ArrayList<Usuario> usuarios;
+    private ArrayList<Usuario> usuarios;   //Aqui é declarada uma variável de instância privada chamada usuarios
+    private String nome;
+    // É uma instância da classe ArrayList que pode armazenar objetos do tipo Usuario.
 
+    // constutor da classe de CadastroUsuarios
     public CadastroUsuarios() {
         usuarios = new ArrayList<>();
     }
 
     public void cadastrarUsuario(String nome, String email) {
-        usuarios.add(new Usuario(nome, email));
+        usuarios.add(new Usuario(nome, email)); // adiciona Novo objeto a lista de produtos usando "Add"
         System.out.println("Usuário cadastrado com sucesso!");
     }
 
     public <Usuario> void listarUsuarios() {
-        if (usuarios.isEmpty()) {
+        if (usuarios.isEmpty()) { // coloquei o metodo isEmpty() para verificar se a lista esta vazia
             System.out.println("Não há usuários cadastrados.");
         } else {
             System.out.println("Usuários cadastrados:");
@@ -26,4 +29,20 @@ public class CadastroUsuarios {
             }
         }
     }
+
+    
+
+    public void excluirUusuario(String nome) {
+        for (int i = 0; i < usuarios.size(); i++){
+            Usuario usuario = usuarios.get(i);
+            if (usuario.getNome().equalsIgnoreCase(nome)){
+                usuarios.remove(i);
+                System.out.println("Usuário excluido com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Usuário não encontrado.");
+        
+    }
+
 }
--- src/Visual/Main.java
+++ src/Visual/Main.java
@@ -12,16 +12,18 @@ public class Main {
 
         do {
             System.out.println("\nSelecione uma opção:");
-            System.out.println("1 - Cadastro de produtos");
-            System.out.println("2 - Cadastro de usuários");
-            System.out.println("3 - Listar produtos");
-            System.out.println("4 - Listar usuários");
+            System.out.println("1 - Cadastro de Produtos");
+            System.out.println("2 - Listar Produtos");
+            System.out.println("3 - Excluir Produtos");
+            System.out.println("4 - Cadastrar Usuários");
+            System.out.println("5 - Listar Usuários");
+            System.out.println("6 - Excluir Usuários");
             System.out.println("0 - Sair");
             System.out.print("Opção: ");
             opcao = scanner.nextInt();
 
             switch (opcao) {
-                case 1:
+                case 1: // cadastrando produto!
                     scanner.nextLine(); // Consumir a quebra de linha
                     System.out.print("Digite o nome do produto: ");
                     String nomeProduto = scanner.nextLine();
@@ -29,7 +31,16 @@ public class Main {
                     double precoProduto = scanner.nextDouble();
                     cadastroProdutos.cadastrarProduto(nomeProduto, precoProduto);
                     break;
-                case 2:
+                case 2: // listando produtos !         ( ✔ )
+                    cadastroProdutos.listarProdutos();
+                    break;
+                case 3: // Excluir produtos !     ( X )
+                    scanner.nextLine(); // Consumir a quebra de linha
+                    System.out.print("Digite o nome do produto a ser excluído: ");
+                    Object nome = scanner.nextLine();
+                    cadastroProdutos.excluirProduto((String) nome);
+                    break;
+                case 4: // Cadastrar Usuário!    ( ✔ )
                     scanner.nextLine(); // Consumir a quebra de linha
                     System.out.print("Digite o nome do usuário: ");
                     String nomeUsuario = scanner.nextLine();
@@ -37,12 +48,14 @@ public class Main {
                     String emailUsuario = scanner.nextLine();
                     cadastroUsuarios.cadastrarUsuario(nomeUsuario, emailUsuario);
                     break;
-                case 3:
-                    cadastroProdutos.listarProdutos();
+                case 5: // Listar Usuário!    ( ✔ )
+                   cadastroUsuarios.listarUsuarios();
                     break;
-                case 4:
-                    cadastroUsuarios.listarUsuarios();
-                    break;
+                case 6: // Excluir Usuário!     ( ✔ )
+                    scanner.nextLine(); // Consumir a quebra de linha
+                    System.out.print("Digite o nome do Usuário a ser excluído: ");
+                    nomeUsuario = scanner.nextLine();
+                    cadastroUsuarios.excluirUusuario(nomeUsuario);
                 case 0:
                     System.out.println("Saindo...");
                     break;
